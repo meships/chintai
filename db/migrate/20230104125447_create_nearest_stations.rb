8ca63de06024829ad191a09d0d9a15100bc70a52
qz <meshipsmou@yahoo.co.jp>
2023-01-05 10:58:13 +0900

    db設計

--- db/migrate/20230104125447_create_nearest_stations.rb
+++ db/migrate/20230104125447_create_nearest_stations.rb
@@ -1,7 +1,10 @@
 class CreateNearestStations < ActiveRecord::Migration[6.0]
   def change
     create_table :nearest_stations do |t|
-
+      t.string :line
+      t.string :station_name
+      t.integer :walk_by
+      
       t.timestamps
     end
   end
--- db/migrate/20230104125818_create_properties.rb
+++ db/migrate/20230104125818_create_properties.rb
@@ -1,6 +1,11 @@
 class CreateProperties < ActiveRecord::Migration[6.0]
   def change
     create_table :properties do |t|
+      t.string :name
+      t.integer :price
+      t.text :address
+      t.integer :age
+      t.text :comment
 
       t.timestamps
     end
