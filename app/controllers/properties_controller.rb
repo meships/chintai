b8a86f57ce90ef34ffb629bc2db30a452fccd48e
qz <meshipsmou@yahoo.co.jp>
2023-01-07 02:20:30 +0900

    大枠完成

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,16 +1,18 @@
 class PropertiesController < ApplicationController
-  before_action :set_property, only: %i[ show edit update destroy] 
-  #before_action :nearest_stations, only: %i[ show edit update]
+
+  before_action :set_property, only: %i[ show edit update destroy ]
+
   def index
     @properties = Property.all
   end
 
-  def new
-    @property = Property.new
-    2.times { @property.nearest_stations.build }
+  def show
+    @nearest_stations = @property.nearest_stations
   end
 
-  def show
+  def new
+    @property = Property.new
+    2.times{@property.nearest_stations.build}
   end
 
   def edit
@@ -19,30 +21,33 @@ class PropertiesController < ApplicationController
 
   def create
     @property = Property.new(property_params)
-      if @property.save
-        redirect_to @property, notice: "物件を登録しました" 
-      else
-        render :new
-      end
+    if @property.save
+      redirect_to properties_path, notice: "投稿作成しました！"
+    else
+      render :new
+    end
   end
 
   def update
+    if @property.update(property_params)
+      redirect_to properties_path notice: "編集しました！"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @property.destroy
+    redirect_to properties_path notice:"削除しました！"
   end
 
   private
-
     def set_property
       @property = Property.find(params[:id])
     end
 
-    def set_nearest_stations
-      @nearest_stations = @property.nearest_stations
+    def property_params
+      params.require(:property).permit(:name, :price, :address, :age, :comment,nearest_stations_attributes:[:id,:line,:station_name,:walk_by])
     end
 
-    def property_params
-      params.require(:property).permit(:name, :price, :address, :age, :comment, nearest_stations_attributes:[:id, :line, :station_name, :walk_by])
-    end
 end
